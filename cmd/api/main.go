4a06ef4ed3176f9046f680b10482579665c6c79c
redmejia <reynaldoemejia@gmail.com>
2025-03-31 14:20:22 -0700

    command flags

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -13,29 +15,37 @@ import (
 
 func main() {
 
+	var port string
+	var host string
+	defaultPort := "8080"
+	defaultHost := "localhost"
+	flag.StringVar(&port, "port", defaultPort, "server port")
+	flag.StringVar(&host, "host", defaultHost, "hostname")
+	flag.Parse()
+
 	var wg sync.WaitGroup
 
 	productList := []product.Product{
-		{ProductID: "PROD-001", Name: "Product 1", Price: "10.00", BarcodeImage: "http://localhost:8080/assets/PROD-001.png"},
-		{ProductID: "PROD-002", Name: "Product 2", Price: "20.00", BarcodeImage: "http://localhost:8080/assets/PROD-002.png"},
-		{ProductID: "PROD-003", Name: "Product 3", Price: "30.00", BarcodeImage: "http://localhost:8080/assets/PROD-003.png"},
-		{ProductID: "PROD-004", Name: "Product 4", Price: "40.00", BarcodeImage: "http://localhost:8080/assets/PROD-004.png"},
-		{ProductID: "PROD-005", Name: "Product 5", Price: "50.00", BarcodeImage: "http://localhost:8080/assets/PROD-005.png"},
-		{ProductID: "PROD-006", Name: "Product 6", Price: "60.00", BarcodeImage: "http://localhost:8080/assets/PROD-006.png"},
-		{ProductID: "PROD-007", Name: "Product 7", Price: "70.00", BarcodeImage: "http://localhost:8080/assets/PROD-007.png"},
-		{ProductID: "PROD-008", Name: "Product 8", Price: "80.00", BarcodeImage: "http://localhost:8080/assets/PROD-008.png"},
-		{ProductID: "PROD-009", Name: "Product 9", Price: "90.00", BarcodeImage: "http://localhost:8080/assets/PROD-009.png"},
-		{ProductID: "PROD-010", Name: "Product 10", Price: "100.00", BarcodeImage: "http://localhost:8080/assets/PROD-010.png"},
-		{ProductID: "PROD-011", Name: "Product 11", Price: "110.00", BarcodeImage: "http://localhost:8080/assets/PROD-011.png"},
-		{ProductID: "PROD-012", Name: "Product 12", Price: "120.00", BarcodeImage: "http://localhost:8080/assets/PROD-012.png"},
-		{ProductID: "PROD-013", Name: "Product 13", Price: "130.00", BarcodeImage: "http://localhost:8080/assets/PROD-013.png"},
-		{ProductID: "PROD-014", Name: "Product 14", Price: "140.00", BarcodeImage: "http://localhost:8080/assets/PROD-014.png"},
-		{ProductID: "PROD-015", Name: "Product 15", Price: "150.00", BarcodeImage: "http://localhost:8080/assets/PROD-015.png"},
-		{ProductID: "PROD-016", Name: "Product 16", Price: "160.00", BarcodeImage: "http://localhost:8080/assets/PROD-016.png"},
-		{ProductID: "PROD-017", Name: "Product 17", Price: "170.00", BarcodeImage: "http://localhost:8080/assets/PROD-017.png"},
-		{ProductID: "PROD-018", Name: "Product 18", Price: "180.00", BarcodeImage: "http://localhost:8080/assets/PROD-018.png"},
-		{ProductID: "PROD-019", Name: "Product 19", Price: "190.00", BarcodeImage: "http://localhost:8080/assets/PROD-019.png"},
-		{ProductID: "PROD-020", Name: "Product 20", Price: "200.00", BarcodeImage: "http://localhost:8080/assets/PROD-020.png"},
+		{ProductID: "PROD-001", Name: "Product 1", Price: "10.00", BarcodeImage: fmt.Sprintf("http://%s:8080/assets/PROD-001.png", host)},
+		{ProductID: "PROD-002", Name: "Product 2", Price: "20.00", BarcodeImage: fmt.Sprintf("http://%s:8080/assets/PROD-002.png", host)},
+		{ProductID: "PROD-003", Name: "Product 3", Price: "30.00", BarcodeImage: fmt.Sprintf("http://%s:8080/assets/PROD-003.png", host)},
+		{ProductID: "PROD-004", Name: "Product 4", Price: "40.00", BarcodeImage: fmt.Sprintf("http://%s:8080/assets/PROD-004.png", host)},
+		{ProductID: "PROD-005", Name: "Product 5", Price: "50.00", BarcodeImage: fmt.Sprintf("http://%s:8080/assets/PROD-005.png", host)},
+		{ProductID: "PROD-006", Name: "Product 6", Price: "60.00", BarcodeImage: fmt.Sprintf("http://%s:8080/assets/PROD-006.png", host)},
+		{ProductID: "PROD-007", Name: "Product 7", Price: "70.00", BarcodeImage: fmt.Sprintf("http://%s:8080/assets/PROD-007.png", host)},
+		{ProductID: "PROD-008", Name: "Product 8", Price: "80.00", BarcodeImage: fmt.Sprintf("http://%s:8080/assets/PROD-008.png", host)},
+		{ProductID: "PROD-009", Name: "Product 9", Price: "90.00", BarcodeImage: fmt.Sprintf("http://%s:8080/assets/PROD-009.png", host)},
+		{ProductID: "PROD-010", Name: "Product 10", Price: "100.00", BarcodeImage: fmt.Sprintf("http://%s:8080/assets/PROD-010.png", host)},
+		{ProductID: "PROD-011", Name: "Product 11", Price: "110.00", BarcodeImage: fmt.Sprintf("http://%s:8080/assets/PROD-011.png", host)},
+		{ProductID: "PROD-012", Name: "Product 12", Price: "120.00", BarcodeImage: fmt.Sprintf("http://%s:8080/assets/PROD-012.png", host)},
+		{ProductID: "PROD-013", Name: "Product 13", Price: "130.00", BarcodeImage: fmt.Sprintf("http://%s:8080/assets/PROD-013.png", host)},
+		{ProductID: "PROD-014", Name: "Product 14", Price: "140.00", BarcodeImage: fmt.Sprintf("http://%s:8080/assets/PROD-014.png", host)},
+		{ProductID: "PROD-015", Name: "Product 15", Price: "150.00", BarcodeImage: fmt.Sprintf("http://%s:8080/assets/PROD-015.png", host)},
+		{ProductID: "PROD-016", Name: "Product 16", Price: "160.00", BarcodeImage: fmt.Sprintf("http://%s:8080/assets/PROD-016.png", host)},
+		{ProductID: "PROD-017", Name: "Product 17", Price: "170.00", BarcodeImage: fmt.Sprintf("http://%s:8080/assets/PROD-017.png", host)},
+		{ProductID: "PROD-018", Name: "Product 18", Price: "180.00", BarcodeImage: fmt.Sprintf("http://%s:8080/assets/PROD-018.png", host)},
+		{ProductID: "PROD-019", Name: "Product 19", Price: "190.00", BarcodeImage: fmt.Sprintf("http://%s:8080/assets/PROD-019.png", host)},
+		{ProductID: "PROD-020", Name: "Product 20", Price: "200.00", BarcodeImage: fmt.Sprintf("http://%s:8080/assets/PROD-020.png", host)},
 	}
 
 	for _, product := range productList {
@@ -45,7 +55,7 @@ func main() {
 	wg.Wait()
 
 	app := &router.App{
-		Port:     ":8080",
+		Port:     fmt.Sprintf(":%s", port),
 		Info:     log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime),
 		ErrorLog: log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile),
 	}
