28bff8eeb7ace9924fe86f852a50b803892787f3
redmejia <reynaldoemejia@gmail.com>
2025-04-01 13:58:48 -0700

    price data type

--- internal/models/product/product.go
+++ internal/models/product/product.go
@@ -1,8 +1,8 @@
 package product
 
 type Product struct {
-	ProductID    string `json:"product_id"`
-	Name         string `json:"name"`
-	Price        string `json:"price"`
-	BarcodeImage string `json:"barcode_image"`
+	ProductID    string  `json:"product_id"`
+	Name         string  `json:"name"`
+	Price        float64 `json:"price"`
+	BarcodeImage string  `json:"barcode_image"`
 }
