b222561467fbfeb0e9869530d45e9cfaa3578d16
redmejia <reynaldoemejia@gmail.com>
2025-04-05 15:39:01 -0700

    logger middleware

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -75,7 +75,7 @@ func main() {
 		Port:     fmt.Sprintf(":%s", port),
 		Info:     log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime),
 		ErrorLog: log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile),
-		// JWTKey:   jwtKey,
+		JWTKey:   jwtKey,
 		// Issuer:      issuer,
 		ProductList: productList,
 		Template:    tmpl,
---
+++ cmd/api/middleware/middleware.go
@@ -0,0 +1,56 @@
+package middleware
+
+import (
+	"encoding/json"
+	"errors"
+	"log"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/redmejia/bos/cmd/api/handlers"
+	sec "github.com/redmejia/bos/internal/utils/security/jwt"
+)
+
+func IsAuthorized(app *handlers.App, next http.HandlerFunc) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		autorization := r.Header.Get("Authorization")
+
+		if len(autorization) > 0 {
+			token := strings.Split(autorization, " ")
+			isValid, _, err := sec.VerifyToken(token[1], app.JWTKey)
+
+			w.Header().Add("Content-Type", "application/json")
+			w.WriteHeader(http.StatusUnauthorized)
+			if err != nil {
+				if errors.Is(err, jwt.ErrTokenExpired) {
+					json.NewEncoder(w).Encode(map[string]string{"error": "Session expired"})
+				} else if errors.Is(err, jwt.ErrTokenMalformed) {
+					json.NewEncoder(w).Encode(map[string]string{"error": "Token malformed"})
+				} else {
+					json.NewEncoder(w).Encode(map[string]string{"error": "Unauthorized"})
+				}
+				return
+			}
+			if isValid {
+				next.ServeHTTP(w, r)
+			}
+		} else {
+			w.Header().Add("Content-Type", "application/json")
+			w.WriteHeader(http.StatusUnauthorized)
+			json.NewEncoder(w).Encode(map[string]string{"error": "Forbidden"})
+			app.ErrorLog.Println("No authorization header")
+		}
+	})
+}
+
+func Logger(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		log.Printf("%s %s %s", r.Method, r.RequestURI, r.Proto)
+		next.ServeHTTP(w, r)
+		log.Println("Request took: ", time.Since(start))
+	})
+}
